c95d3f0aa450f0ba17004b7532c504f398c8a5bf
Florian Uekermann <florian@uekermann.me>
2023-04-23 15:34:27 +0200

    switch to cargo wasm-bundle

--- rust/build.sh
+++ rust/build.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-PROFILE="release"
-ARGS="--release"
-if [[ $* == *--debug* ]]; then
-    PROFILE="debug"
-    ARGS=""
-fi
-
-cargo +nightly build --target wasm32-unknown-unknown ${ARGS}
-wasm-bindgen --target web target/wasm32-unknown-unknown/${PROFILE}/ising.wasm --out-dir target/wasm32-unknown-unknown/${PROFILE}/
-cargo build ${ARGS} --example pack
-(
-    cd target/wasm32-unknown-unknown/${PROFILE}/
-    ../../${PROFILE}/examples/pack
-)
-cp target/wasm32-unknown-unknown/${PROFILE}/main.html ./
+cargo wasm-bundle --release
+cp target/wasm-bundle/release/ising.html ./
